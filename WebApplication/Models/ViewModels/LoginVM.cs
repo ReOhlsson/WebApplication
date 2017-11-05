1fcdc9b63d5dfa7f789f1d916c2d1a28fa31b7bc
ReOhlsson <reto1500@student.miun.se>
2017-11-05 17:13:25 +0100

    View för inloggning

---
+++ WebApplication/Models/ViewModels/LoginVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models.ViewModels
+{
+    public class LoginVM
+    {
+        //Errormessage
+        [Required(ErrorMessage="Du måste fylla i ditt användarnamn!")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Du måste fylla i ditt lösenord!")]
+        //Ser till att inmatningsfältet blir av typen Passsword
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
