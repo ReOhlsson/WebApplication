fdb6a61dad47874b8f91b05a3aa4198677010741
Ida Lundgren <idlu1201@student.miun.se>
2017-11-07 12:46:31 +0100

    ändra lite i css

--- WebApplication/Scripts/getchannels.js
+++ WebApplication/Scripts/getchannels.js
@@ -2,14 +2,14 @@
     var channelName = ["svt1.svt", "tv3", "tv4"];
 
     $.each(channelName, function (index, value) {
-        var $d = $("<div class='col-md-4 number" + index + "'>").load('Home/Shows', { channel: value });
+        var $d = $("<div class='col-md-4 number'>").load('Home/Shows', { channel: value });
         $('.containerChannel').append($d);
     });
 
     var channelName = ["kanal5", "tv6", "tv8"];
 
     $.each(channelName, function (index, value) {
-        var $d = $("<div class='col-md-4 number" + index + "'>").load('Home/Shows', { channel: value });
+        var $d = $("<div class='col-md-4 number'>").load('Home/Shows', { channel: value });
         $('.containerChannel2').append($d);
     });
 
