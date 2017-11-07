df43d5480f5d15afa011b002d5027235e0a55a98
Ida Lundgren <idlu1201@student.miun.se>
2017-11-07 17:21:33 +0100

    Fixat så sidan inte laddas om när man fåt upp popup:en.

--- WebApplication/Scripts/JavaScriptIndex.js
+++ WebApplication/Scripts/JavaScriptIndex.js
@@ -31,28 +31,30 @@ function showPopUp(program) {
 }
 
 function closePopUp() {
+    debugger
     document.getElementById('popupMovie').style.display = "none";
 }
-//$(".programLink").click(function () {
-//    debugger
-//    var program = document.getElementsByClassName('programLink');
-//    var title = program.getAttribute("data-title") // "3"
-//    var start = program.getAttribute("data-start") // "3"
-//    var stop = program.getAttribute("data-stop") // "3"
-//    var desc = program.getAttribute("data-desc") // "3"
-//    var channel = program.getAttribute("data-channel") // "3"
 
-//    var animalType = animal.getAttribute("data-animal-type");
+$(".programLink").click(function () {
+    debugger
+    var program = document.getElementsByClassName('programLink');
+    var title = program.getAttribute("data-title") // "3"
+    var start = program.getAttribute("data-start") // "3"
+    var stop = program.getAttribute("data-stop") // "3"
+    var desc = program.getAttribute("data-desc") // "3"
+    var channel = program.getAttribute("data-channel") // "3"
+
+    var animalType = animal.getAttribute("data-animal-type");
 
 
-//    $.ajax({
-//        type: "GET",
-//        url: "/home/showspop",
-//        data: { title:title , start :start, stop:stop, desc :desc, channel: channel },
-//        async: false,
-//        success: function (data) {
-//            $('#programPopup' + index).html(data);
-//        }
-//    });
-//});
+    $.ajax({
+        type: "GET",
+        url: "/home/showspop",
+        data: { title:title , start :start, stop:stop, desc :desc, channel: channel },
+        async: false,
+        success: function (data) {
+            $('#programPopup' + index).html(data);
+        }
+    });
+});
 
