567acdc1851e5a092518f1765a46df7aca4f4561
Ida Lundgren <idlu1201@student.miun.se>
2017-11-07 20:52:46 +0100

    funkar nästan att markera gamla program

--- WebApplication/Scripts/JavaScriptIndex.js
+++ WebApplication/Scripts/JavaScriptIndex.js
@@ -55,14 +55,14 @@ $(".programLink").click(function () {
     });
 });
 
-$(document).ready(function () {
+$(window).on('load', function () {
     var date = Math.round((new Date()).getTime() / 1000);
     debugger
     $(".program li a").each(function (key, value) {
         var unix = value.getAttribute("data-start");
         var a = parseInt(unix)
 
-        if (date > a) {
+        if (date >= a) {
             this.style.background = "pink";
         }
     });
