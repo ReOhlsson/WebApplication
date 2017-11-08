7520c48ed616269f13b8cf11dc675c59ae326495
Ida Lundgren <idlu1201@student.miun.se>
2017-11-08 18:36:50 +0100

    Förberätt för en registrering

--- WebApplication/Controllers/AccountController.cs
+++ WebApplication/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using RepoAndUnitOfWork.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
@@ -47,6 +48,34 @@ namespace WebApplication.Controllers
 
             FormsAuthentication.SignOut();
             return RedirectToAction("Index", "Home");
+        }
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(LoginVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                //var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                //var result = await UserManager.CreateAsync(user, model.Password);
+                //if (result.Succeeded)
+                //{
+                //    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                //    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                //    // Send an email with this link
+                //    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                //    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                //    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                //    return RedirectToAction("Index", "Home");
+                //}
+                //AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
         }
     }
 }
